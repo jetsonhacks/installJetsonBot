5137c5e0f2825cf26fad53113a6bbd71c98a730b
jetsonhacks <jim@jetsonhacks.com>
2015-07-04 10:57:20 -0700

    JetsonBot export variables, prerequisites

---
+++ exportVariables.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Set the export variables for JetsonBot
+export TURTLEBOT_BASE=create2
+export TURTLEBOT_STACKS=circles
+export TURTLEBOT_3D_SENSOR=asus_xtion_pro
+export TURTLEBOT_SERIAL_PORT=/dev/ttyUSB0
+
--- installJetsonBot.sh
+++ installJetsonBot.sh
@@ -4,7 +4,13 @@ cd ~/
 _dir="jetsonbot/src"
 # Check to make sure that the JetsonBot catkin directory exists
 [ ! -d "$_dir" ] && { echo "Error: Directory $_dir not found.\nPlease run createJetsonBotWS.sh to create the JetsonBot catkin workspace."; exit 2; }
+# Install TurtleBot from repository
+sudo apt-get install ros-indigo-turtlebot ros-indigo-turtlebot-apps ros-indigo-turtlebot-interactions ros-indigo-rocon-remocon ros-indigo-rocon-qt-library ros-indigo-ar-track-alvar-msgs -y
+
 cd ~/jetsonbot/src
 git clone https://github.com/jetsonhacks/turtlebot.git
 git clone https://github.com/jetsonhacks/turtlebot_create.git
+cd ..
+catkin_make
+
 
