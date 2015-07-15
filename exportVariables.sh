2c81e244fb739dc50043f55e7f0b95146ccb4c29
jetsonhacks <jim@jetsonhacks.com>
2015-07-15 16:36:50 -0700

    Add TurtleBot environment variables

--- exportVariables.sh
+++ exportVariables.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # Set the export variables for JetsonBot
-export TURTLEBOT_BASE=create2
-export TURTLEBOT_STACKS=circles
-export TURTLEBOT_3D_SENSOR=asus_xtion_pro
-export TURTLEBOT_SERIAL_PORT=/dev/ttyUSB0
-
+# This script replicates part of installJetsonBot.sh, and should just be used for
+# informational purposes
+# These variables tell ROS the TurtleBot configuration;
+echo export TURTLEBOT_BASE=create2 >> ~/.bashrc
+echo export TURTLEBOT_STACKS=circles >> ~/.bashrc
+echo export TURTLEBOT_3D_SENSOR=asus_xtion_pro >> ~/.bashrc
+echo export TURTLEBOT_SERIAL_PORT=/dev/ttyUSB0 >> ~/.bashrc
+# Use the JetsonBot development paths before using the default
+echo source ~/jetsonbot/devel/setup.bash >> ~/.bashrc
+# The JetsonBot is the ROS_MASTER
+echo export ROS_MASTER_URI=http://localhost:11311 >> ~/.bashrc
+# The ROS_HOSTNAME may need to be set
+# export ROS_HOSTNAME=jetsonbot
+# or
+# export ROS_IP=<jetsonbot IP>
--- installJetsonBot.sh
+++ installJetsonBot.sh
@@ -10,7 +10,20 @@ sudo apt-get install ros-indigo-turtlebot ros-indigo-turtlebot-apps ros-indigo-t
 cd ~/jetsonbot/src
 git clone https://github.com/jetsonhacks/turtlebot.git
 git clone https://github.com/jetsonhacks/turtlebot_create.git
+git clone https://github.com/jetsonhacks/turtlebot_interactions.git
 cd ..
 catkin_make
-
-
+# These variables tell ROS the TurtleBot configuration;
+echo export TURTLEBOT_BASE=create2 >> ~/.bashrc
+echo export TURTLEBOT_STACKS=circles >> ~/.bashrc
+echo export TURTLEBOT_3D_SENSOR=asus_xtion_pro >> ~/.bashrc
+echo export TURTLEBOT_SERIAL_PORT=/dev/ttyUSB0 >> ~/.bashrc
+# Use the JetsonBot development paths before using the default
+echo source ~/jetsonbot/devel/setup.bash >> ~/.bashrc
+# The JetsonBot is the ROS_MASTER
+echo export ROS_MASTER_URI=http://localhost:11311 >> ~/.bashrc
+# The ROS_HOSTNAME may need to be set
+# export ROS_HOSTNAME=jetsonbot
+# or
+# export ROS_IP=<jetsonbot IP>
+/bin/echo -e "\e[1;32mJetsonBot Software Installed.\e[0m"
